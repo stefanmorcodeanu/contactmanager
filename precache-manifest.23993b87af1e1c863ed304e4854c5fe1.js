e357ae46ad8d131c8050d1455f7c66625bfaa4d6
Ștefan Morcodeanu <stefanmorcodeanu@gmail.com>
2019-07-30 00:23:07 +0300

    Updates

---
+++ precache-manifest.23993b87af1e1c863ed304e4854c5fe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2871e2f327182dad2cc3ec734be950",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "f748b45e661c527f5be3",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "92246910a592e3b8d890",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f748b45e661c527f5be3",
+    "url": "/contactmanager/static/js/2.f96624a1.chunk.js"
+  },
+  {
+    "revision": "92246910a592e3b8d890",
+    "url": "/contactmanager/static/js/main.9965e838.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
